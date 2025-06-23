aa3e280703e53b8d371b4449beb489cd49dc493d
Ponpreeth <ponpreeth@gmail.com>
2025-06-23 11:51:00 +0530

    New Learnings

---
+++ index.js
@@ -0,0 +1,12 @@
+const addNewCard = () => {
+    // get task data
+    const taskData ={
+       id:'${date.now()}',
+       title:document.getElementById("task title").value,
+       image:document.getElementById("imageurl").value,
+       type:document.getElementById("tasktype").value,
+       descripition:document.getElementById("Task description").value,
+    };
+    
+    
+};
